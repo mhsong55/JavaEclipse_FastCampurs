aa5aa864c624e16832e758d831710d4845a6a40c
MHSong <58389348+SongMyungHoon@users.noreply.github.com>
2020-04-14 12:10:42 +0900

    change preference setting
    encoding type change to UTF 8

--- Chapter5/src/classpart/StudentTest.java
+++ Chapter5/src/classpart/StudentTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package classpart;
 
 public class StudentTest {
@@ -21,3 +22,28 @@ public class StudentTest {
 	}
 
 }
+=======
+package classpart;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student studentLee = new Student();
+		studentLee.studentName = "이순신";
+		studentLee.address = "서울";
+		
+		studentLee.showStudentInfo();
+		
+		Student studentKim = new Student();
+		studentKim.studentName = "김유신";
+		studentKim.address = "경주";
+		
+		studentKim.showStudentInfo();
+		
+		System.out.println(studentLee);
+		System.out.println(studentKim);
+	}
+
+}
+>>>>>>> 2abaf935dab07168daea6336a9be4ba3041b60df
